42d9a2f4b24e0f398ce62ac03cb52d435cda7155
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-06-02 12:38:04 +0100

    Add switch statement practice

--- src/main/java/com/zinkworks/Main.java
+++ src/main/java/com/zinkworks/Main.java
@@ -3,5 +3,46 @@ package com.zinkworks;
 public class Main {
     public static void main(String[] args) {
 
+        intSwitch(4);
+        stringSwitch("February");
+    }
+
+    public static void intSwitch(int switchValue) {
+
+        switch(switchValue) {
+            case 1:
+                System.out.println("Value is 1");
+                break;
+            case 2:
+                System.out.println("Value is 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("Value was " + switchValue);
+                //break;
+            default:
+                System.out.println("Value is not 1 or 2");
+                break;
+        }
+    }
+
+    public static void stringSwitch(String switchValue) {
+
+        switch(switchValue.toLowerCase()) {
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "february":
+                System.out.println("Feb");
+                break;
+            case "march":
+                System.out.println("Mar");
+                break;
+            case "april":
+                System.out.println("Apr");
+                break;
+            default:
+                System.out.println("Value is not Jan to Apr");
+                break;
+        }
     }
 }
